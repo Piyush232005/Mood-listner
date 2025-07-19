8e047c1782ece7a7f8766c7884e92d1a4a1e1b45
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-07-20 00:34:40 +0530

    first commit of listener

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import FacialExpression from "./components/FacialExpression";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <FacialExpression/>
+    </>
+  )
+}
+
+export default App
