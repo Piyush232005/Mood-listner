2045f71dd57f492f217b91a73f8d6d45af3a6cf2
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-07-29 23:42:50 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const connectDB = require('./src/db/db');
 connectDB();
 // Start the server after connecting to the database
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 }); 
